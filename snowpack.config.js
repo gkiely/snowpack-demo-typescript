1bc3f57a749f515e1dd28205b72dd05903b71326
Grant <grant.kiely@gmail.com>
2021-01-16 22:31:48 -0800

    updated snowpack

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,20 +1,25 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    src: "/"
+    src: '/',
   },
   devOptions: {
-    port: 8000
+    port: 8000,
   },
   plugins: [
     '@snowpack/plugin-react-refresh',
     '@snowpack/plugin-dotenv',
     '@snowpack/plugin-typescript',
     [
-      "@snowpack/plugin-optimize", {
-        preloadCSS: true
-      }
+      '@snowpack/plugin-optimize',
+      {
+        preloadCSS: true,
+      },
     ],
-    "snowpack-plugin-skypack-replacer",
+    'snowpack-plugin-skypack-replacer',
   ],
+  // packageOptions: {
+  //   source: 'remote',
+  //   types: true,
+  // },
 };
--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,21 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 type Props = {
   count: number;
+};
+
+type WeakMap = {
+  [k: string]: any;
+};
+
+function Component({ count }: Props) {
+  return <div className="App">Page has been open for {count} seconds.</div>;
 }
 
-function Component({count}: Props){  
-  return (
-    <div className="App">
-      Page has been open for {count} seconds.
-    </div>
-  );
-}
-
-export default function Container({startTime = 0}){
+export default function Container({ startTime = 0 }) {
   const [count, setCount] = useState(startTime);
   useEffect(() => {
     const timer = setTimeout(() => setCount(count + 1), 1000);
     return () => clearTimeout(timer);
   }, [count, setCount]);
-  return <Component count={count} />
+  return <Component count={count} />;
 }
